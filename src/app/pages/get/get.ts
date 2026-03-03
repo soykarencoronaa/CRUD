3c54330741ec32e3abc1e070e7e1a6e21bd34d8c
soykarencoronaa <l22200722@pachuca.tecnm.mx>
2026-03-03 11:47:24 -0600

    Agregar carpetas

---
+++ src/app/pages/get/get.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get',
+  imports: [],
+  templateUrl: './get.html',
+  styleUrl: './get.css',
+})
+export class Get {
+
+}
